f7c20c7df433ca2b8a1ef58781321f63aa3d5439
Scorredorcontreras <corredorcontreras1204@gmail.com>
2025-05-20 20:58:46 -0500

    Arreglo en la funcion del loggin para que tenga la recion con el rol del usuario

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { ConexionModule } from 'src/config/conexion/conexion.module';
 
 @Module({
   controllers: [UsersController],
